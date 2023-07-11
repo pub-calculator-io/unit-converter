3bb9df47f7aa6153136f224fb775a887fbfc6e60
Arseniy Olevskiy <77734966+arolus@users.noreply.github.com>
2023-07-11 08:54:37 +0500

    regular commit

--- plugin.php
+++ plugin.php
@@ -10,11 +10,11 @@ License: GPLv2 or later
 Text Domain: ci_unit_converter
 */
 
-if (!function_exists('add_shortcode')) die("No direct call");
+if (!function_exists('add_shortcode')) return "No direct call for Unit Converter by Calculator.iO";
 
 function display_ci_unit_converter(){
     $page = 'index.html';
-    return '<h2><a href="https://www.calculator.io/unit-converter/" target="_blank"><img src="' . plugins_url('assets/images/icon-48.png', __FILE__ ) . '" width="48" height="48"></a> Unit Converter</h2><div><iframe style="background:transparent; overflow: scroll" src="' . plugins_url($page, __FILE__ ) . '" width="100%" frameBorder="0" allowtransparency="true" onload="this.style.height = this.contentWindow.document.documentElement.scrollHeight + \'px\';" id="ci_unit_converter_iframe"></iframe></div>';
+    return '<h2><a href="https://www.calculator.io/unit-converter/" target="_blank"><img src="' . esc_url(plugins_url('assets/images/icon-48.png', __FILE__ )) . '" width="48" height="48"></a> Unit Converter</h2><div><iframe style="background:transparent; overflow: scroll" src="' . esc_url(plugins_url($page, __FILE__ )) . '" width="100%" frameBorder="0" allowtransparency="true" onload="this.style.height = this.contentWindow.document.documentElement.scrollHeight + \'px\';" id="ci_unit_converter_iframe"></iframe></div>';
 }
 
 add_shortcode( 'ci_unit_converter', 'display_ci_unit_converter' );
